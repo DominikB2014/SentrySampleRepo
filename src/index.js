4fa7f0579db425c8dfa8f34caa3f2e87f0d1403a
Dominik Buszowiecki <dominikbuszowiecki@Dominiks-MBP.localdomain>
2022-06-15 10:33:10 -0400

    create basic sentry app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from "@sentry/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+Sentry.init({
+  dsn: "https://b2ab050f3ad54b5aadded1af46793339@o1288502.ingest.sentry.io/6507186",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
